6d78f16b514877f8389b15b01426f7685ad6cdc5
Kjeld Flindt Ejlerskov <kjel0737@skivecollege.dk>
2019-05-28 09:18:50 +0200

    DBContext User Added

---
+++ AspitAktivitet/Healpers/DB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using AspitAktivitet.Models;
+
+namespace AspitAktivitet.Healpers
+{
+    public class DB : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
