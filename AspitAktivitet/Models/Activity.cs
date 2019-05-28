175534dea5c9f08e429909f69e73a6ebfaf0d817
Oliver Lind Nielsen <oliv2294@skivecollege.dk>
2019-05-28 13:47:00 +0200

    blab 2

---
+++ AspitAktivitet/Models/Activity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspitAktivitet.Models
+{
+    public class Activity
+    {
+        public int ID { get; set; }
+        [StringLength(50)]
+        [Index(IsUnique = true)]
+        public string Name { get; set; }
+    }
+}
