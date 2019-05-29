a5251dc647075b77ddcc1861b7dab29ead208c8d
Kjeld Flindt Ejlerskov <kjel0737@skivecollege.dk>
2019-05-29 09:01:38 +0200

    mere hjælp

---
+++ AspitAktivitet/Models/Planned.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspitAktivitet.Models
+{
+    public class Planned
+    {
+        [Key, Column(Order = 1)]
+        public int ActivtyID { get; set; }
+
+        [Key, Column(Order = 0)]
+        public DateTime Date { get; set; }
+    }
+}
